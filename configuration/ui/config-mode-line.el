40eb99626c1f77dfdbff86f1d077a270584bfc63
João Guerra <joca.bt@gmail.com>
2015-02-01 19:29:14 +0000

    Fuck powerline.

--- configuration/ui/config-mode-line.el
+++ configuration/ui/config-mode-line.el
@@ -3,28 +3,19 @@
 ;;
 ;; based on Emacs' bindings.el
 
-(setq-default mode-line-format '((:eval (ml-powerline))))
+(setq-default mode-line-format '(" "
+                                 (:eval (ml-encoding-and-terminator))
+                                 "  "
+                                 (:eval (ml-position))
+                                 "  "
+                                 (:eval (ml-buffer-status))
+                                 "  "
+                                 (:eval (ml-buffer-name))
+                                 "  "
+                                 (:eval (ml-major-mode))
+                                 (:eval (ml-minor-modes))))
 
 (setq mode-line-default-help-echo nil)
-
-(defun ml-powerline ()
-  (let ((active-p (powerline-selected-window-active))
-        (lhs (list (powerline-raw " ")
-                   (powerline-raw (ml-encoding-and-terminator))
-                   (powerline-raw "  ")
-                   (powerline-raw (ml-position))
-                   (powerline-raw "  ")
-                   (powerline-raw (ml-buffer-status))
-                   (powerline-raw "  ")
-                   (powerline-raw (ml-buffer-name))
-                   (powerline-raw "  ")
-                   (powerline-raw (ml-major-mode))
-                   (powerline-raw " ")
-                   (powerline-raw (ml-minor-modes))))
-        (rhs (list (powerline-raw " "))))
-    (concat (powerline-render lhs)
-            (powerline-fill 'powerline-active1 (powerline-width rhs))
-            (powerline-render rhs))))
 
 
 ;; -----------------------------------------------------------------------------
@@ -135,11 +126,11 @@
 ;; major mode ------------------------------------------------------------------
 
 (defun ml-major-mode ()
-  (propertize (format-mode-line mode-name)
-              'mouse-face 'ml-highlight
-              'pointer 'arrow
-              'help-echo 'ml-major-mode-help
-              'local-map ml-major-mode-map))
+  `(:propertize ("" mode-name)
+                mouse-face ml-highlight
+                pointer arrow
+                help-echo ml-major-mode-help
+                local-map ,ml-major-mode-map))
 
 (defun ml-major-mode-help (window object point)
   (with-selected-window window
@@ -167,32 +158,20 @@
 (eval-after-load "ws-trim" '(diminish 'ws-trim-mode))
 
 (defun ml-minor-modes ()
-  (let ((minor-modes (mapcar #'(lambda (mode)
-                                 (propertize mode
-                                             'mouse-face 'ml-highlight
-                                             'pointer 'arrow
-                                             'help-echo 'ml-minor-mode-help
-                                             'local-map (ml-minor-mode-map mode)))
-                             (split-string (format-mode-line minor-mode-alist)))))
-    (butlast (cl-merge 'list
-                       minor-modes
-                       (make-list (length minor-modes) " ")
-                       (let ((interleaver t))
-                         #'(lambda (&rest args)
-                             (setf interleaver (not interleaver))))))))
+  `(:propertize ("" minor-mode-alist)
+                mouse-face ml-highlight
+                pointer arrow
+                help-echo ml-minor-mode-help
+                local-map ,ml-minor-mode-map))
 
 (defun ml-minor-mode-help (window object point)
   (with-selected-window window
     "Minor mode\nmouse-1: Show minor mode menu\nmouse-2: Describe minor mode\nmouse-3: Toggle minor modes"))
 
-(defun ml-minor-mode-map (mode)
+(defvar ml-minor-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map [mode-line mouse-1] #'(lambda (event)
-                                            (interactive "@e")
-                                            (minor-mode-menu-from-indicator mode)))
-    (define-key map [mode-line mouse-2] #'(lambda (event)
-                                            (interactive "@e")
-                                            (describe-minor-mode-from-indicator mode)))
+    (define-key map [mode-line mouse-1] 'mouse-minor-mode-menu)
+    (define-key map [mode-line mouse-2] 'mode-line-minor-mode-help)
     (define-key map [mode-line mouse-3] mode-line-mode-menu)
     (purecopy map)))
 
