b74a32f3778cd64135ec67c21e85c06b101125f4
João Guerra <joca.bt@gmail.com>
2019-10-09 21:07:26 +0200

    Refactor set-file-tabs.el

--- settings/ui/set-file-tabs.el
+++ settings/ui/set-file-tabs.el
@@ -1,4 +1,4 @@
-;; -----------------------------------------------------------------------------
+;; -------------------------------------------------- -*- lexical-binding: t -*-
 ;; file tabs -------------------------------------------------------------------
 
 (defface tabbar-selected-highlight '((t))
@@ -15,30 +15,31 @@
                 (buffer-list)))
 
 (defun tabbar-line-buttons (tabset)
-  (list ""))
+  '(""))
 
-(defun tabbar-line-tab (tab)
-  (let* ((buffer (tabbar-tab-value tab))
-         (selected-p (tabbar-selected-p tab (tabbar-current-tabset)))
-         (label (if tabbar-tab-label-function
-                    (funcall tabbar-tab-label-function tab)
-                  tab))
-         (face (if selected-p
-                   'tabbar-selected
-                 'tabbar-unselected))
-         (mouse-face (if selected-p
-                         'tabbar-selected-highlight
-                       'tabbar-unselected-highlight)))
-    (concat (propertize (format "%s%s%s" (make-space 1) label (make-space 1))
-                        'tabbar-tab tab
-                        'face face
-                        'mouse-face mouse-face
-                        'pointer 'arrow
-                        'help-echo #'tabbar-line-tab-help
-                        'local-map (tabbar-make-tab-keymap tab))
-            (if selected-p
-                (make-xpm 30 1 (face-attribute 'tabbar-separator :background))
-              tabbar-separator-value))))
+(let ((separator-selected (make-xpm 30 1 (face-attribute 'tabbar-separator :background)))
+      (separator-unselected (make-space 0.1 'tabbar-separator)))
+  (defun tabbar-line-tab (tab)
+    (let* ((selected-p (tabbar-selected-p tab tabbar-current-tabset))
+           (face (if selected-p
+                     'tabbar-selected
+                   'tabbar-unselected))
+           (mouse-face (if selected-p
+                           'tabbar-selected-highlight
+                         'tabbar-unselected-highlight)))
+      (concat (propertize (format "%s%s%s"
+                                  (make-space 1)
+                                  (tabbar-buffer-tab-label tab)
+                                  (make-space 1))
+                          'tabbar-tab tab
+                          'face face
+                          'mouse-face mouse-face
+                          'pointer 'arrow
+                          'help-echo #'tabbar-line-tab-help
+                          'local-map (tabbar-make-tab-keymap tab))
+              (if selected-p
+                  separator-selected
+                separator-unselected)))))
 
 (defun tabbar-line-tab-help (window object position)
   (with-selected-window window
@@ -50,11 +51,10 @@
         (buffer-name buffer)))))
 
 (defun tabbar-update (&rest args)
-  (tabbar-set-template (tabbar-current-tabset) nil)
+  (tabbar-set-template tabbar-current-tabset nil)
   (tabbar-display-update))
 
-(setq tabbar-buffer-groups-function nil
-      tabbar-separator '(0.1))
+(setq tabbar-buffer-groups-function nil)
 (tabbar-mode t)
 (tabbar-mwheel-mode -1)
 
