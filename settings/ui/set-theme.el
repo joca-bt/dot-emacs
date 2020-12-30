8b9f0a546a67eff36c15d7a0f9e70b0f24dbce04
João Guerra <joca-bt@users.noreply.github.com>
2020-12-30 10:15:47 +0100

    Update theme

--- settings/ui/set-theme.el
+++ settings/ui/set-theme.el
@@ -1,29 +1,30 @@
 ;; -----------------------------------------------------------------------------
 ;; theme -----------------------------------------------------------------------
 
-(setq custom-theme-directory +themes-dir+)
+(setq custom--inhibit-theme-enable nil
+      custom-theme-directory +themes-dir+)
 (load-theme 'ample t)
 
 (custom-theme-set-faces 'ample
                         ;; emacs
                         '(fringe ((t)))
                         '(line-number-current-line ((t)))
-                        '(vertical-border ((t :foreground "#454545")))
-                        '(isearch ((t :foreground "gray13" :background "#5180b3" :underline nil)))
+                        '(vertical-border ((t :foreground "gray27")))
+                        '(isearch ((t :background "#5180b3" :foreground "gray13" :underline nil)))
                         '(match ((t :inherit isearch)))
                         ;; file tabs
-                        `(tabbar-default ((t ,@+ui-font+ :foreground "black" :background "grey80")))
-                        '(tabbar-selected ((t :inherit tabbar-default :foreground "grey15" :background "grey95")))
+                        '(tabbar-default ((t :inherit variable-pitch :height 0.9 :background "grey85" :foreground "black")))
+                        '(tabbar-selected ((t :inherit tabbar-default)))
                         '(tabbar-selected-highlight ((t :inherit tabbar-selected)))
-                        '(tabbar-unselected ((t :inherit tabbar-default)))
-                        '(tabbar-unselected-highlight ((t :inherit tabbar-unselected :background "grey65")))
-                        '(tabbar-separator ((t :inherit tabbar-default :background "grey50")))
+                        '(tabbar-unselected ((t :inherit tabbar-default :background "grey75")))
+                        '(tabbar-unselected-highlight ((t :inherit tabbar-selected-highlight)))
+                        '(tabbar-separator ((t :inherit tabbar-default :background "grey55")))
                         ;; mode line
                         '(ml-buffer-name ((t :weight bold)))
-                        '(ml-active-window-highlight ((t :foreground "cornsilk4" :background "#454545")))
-                        '(ml-inactive-window-highlight ((t :foreground "#252525" :background "cornsilk4")))
+                        '(ml-active-window-highlight ((t :inherit mode-line-inactive)))
+                        '(ml-inactive-window-highlight ((t :inherit mode-line)))
                         ;; ivy
-                        '(ivy-current-match ((t :background "gray17")))
+                        '(ivy-current-match ((t :background "gray17" :extend t)))
                         '(ivy-minibuffer-match-face-1 ((t)))
                         '(ivy-minibuffer-match-face-2 ((t :inherit flx-highlight-face)))
                         '(ivy-minibuffer-match-face-3 ((t :inherit flx-highlight-face)))
