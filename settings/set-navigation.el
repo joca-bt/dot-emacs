9f75497bbd0b8a3b11528d824c5eb6fb91190d2d
João Guerra <joca-bt@users.noreply.github.com>
2020-12-30 12:58:39 +0100

    Update navigation

--- settings/set-navigation.el
+++ settings/set-navigation.el
@@ -1,15 +1,8 @@
 ;; -----------------------------------------------------------------------------
 ;; navigation ------------------------------------------------------------------
 
-(defun transpose-windows ()
-  (interactive)
-  (let* ((window1 (selected-window))
-         (window2 (next-window window1))
-         (buffer1 (window-buffer window1))
-         (start1 (window-start window1))
-         (point1 (window-point window1)))
-    (set-window-buffer-start-and-point window1 (window-buffer window2) (window-start window2) (window-point window2))
-    (set-window-buffer-start-and-point window2 buffer1 start1 point1)))
+;; isearch
+(setq isearch-allow-scroll t)
 
 ;; -----------------------------------------------------------------------------
 ;; -----------------------------------------------------------------------------
